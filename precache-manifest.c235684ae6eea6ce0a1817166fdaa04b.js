7743cdfc8d1060a3553618b1c738b2e2c999d038
Anmol Mathias <anmolmathias@gmail.com>
2022-11-06 21:33:31 +0530

    Updates

---
+++ precache-manifest.c235684ae6eea6ce0a1817166fdaa04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d747bbee28e633232b7",
+    "url": "/robofriends/static/css/main.ad5b11df.chunk.css"
+  },
+  {
+    "revision": "0d747bbee28e633232b7",
+    "url": "/robofriends/static/js/main.0d747bbe.chunk.js"
+  },
+  {
+    "revision": "669168012a5106f4e076",
+    "url": "/robofriends/static/css/1.6d3652d9.chunk.css"
+  },
+  {
+    "revision": "669168012a5106f4e076",
+    "url": "/robofriends/static/js/1.66916801.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7e821847bb5bb03c2c4754a9926bc2c0",
+    "url": "/robofriends/index.html"
+  }
+];
